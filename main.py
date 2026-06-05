7b35415d9ed87766dca40ba42fd4807adcd993a6
Mo~ <mohyee.rageb@coalitiontechnologies.com>
2026-06-05 20:26:47 +0300

    Allow platform previews to use All In One API

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.add_middleware(
         "https://copypilot.app",
         "https://all-in-one.copypilot.app",
     ],
+    allow_origin_regex=r"https://copypilot-platform(?:-[a-z0-9-]+)?-mohyeects-projects\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
